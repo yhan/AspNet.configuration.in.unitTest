a1cac5fbab627bb97b8315e3217b7292bc206637
han <yhan@la-francaise.com>
2018-11-14 19:16:33 +0100

    Using Configuration files in .NET Core Unit Test Projects

---
+++ AspNet.configuration.in.unitTest/ConfigurationHelperForTesting.cs
@@ -0,0 +1,36 @@
+﻿namespace AspNet.configuration.inside.unitTest
+{
+    using Microsoft.Extensions.Configuration;
+
+    public class ConfigurationHelperForTesting
+    {
+        public static IConfigurationRoot GetApplicationConfigurationRoot(string outputPath)
+        {
+            return new ConfigurationBuilder().SetBasePath(outputPath).AddJsonFile("appsettings.Tests.json", true).AddEnvironmentVariables().Build();
+        }
+
+        public static CaraibesConfigs GetApplicationConfiguration(string outputPath)
+        {
+            var configuration = new CaraibesConfigs();
+
+            var iConfig = GetApplicationConfigurationRoot(outputPath);
+
+            iConfig
+
+                // .GetSection("ReadProjector")
+                .Bind(configuration);
+
+            return configuration;
+        }
+    }
+
+    public class ReadProjector
+    {
+        public string ProjectionVersion { get; set; }
+    }
+
+    public class CaraibesConfigs
+    {
+        public ReadProjector ReadProjector { get; set; }
+    }
+}
---
+++ AspNet.configuration.in.unitTest/ConfigurationShould.cs
@@ -0,0 +1,54 @@
+﻿namespace AspNet.configuration.inside.unitTest
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using NFluent;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ConfigurationShould
+    {
+        [SetUp]
+        public void Init()
+        {
+            var testDirectory = TestContext.CurrentContext.TestDirectory;
+            OutputPath = $"{testDirectory}\\Configuration";
+            ConfigurationRoot = ConfigurationHelperForTesting.GetApplicationConfigurationRoot(OutputPath);
+        }
+
+        [Test]
+        public void Can_get_read_projection_version_value()
+        {
+            Check.That(ConfigurationRoot["ProjectionVersionFromConfig"]).IsEqualTo("v1");
+        }
+
+        [Test]
+        public void Can_get_read_projection_version_value_from_strongly_typed_CaraibesConfig()
+        {
+            var readProjector = ConfigurationHelperForTesting.GetApplicationConfiguration(OutputPath);
+            Check.That(readProjector.ReadProjector.ProjectionVersion).IsEqualTo("v2");
+        }
+
+        [Test]
+        public void Can_get_config_from_options()
+        {
+            var services = new ServiceCollection();
+
+            // IOption configuration injection
+            services.AddOptions();
+
+            var caraibesConfigs = new CaraibesConfigs();
+            var configurationSection = ConfigurationRoot.GetSection("Weblog");
+
+            services.Configure<CaraibesConfigs>(caConfigs => { configurationSection.Bind(caraibesConfigs); });
+
+            // TODO: then what? What is the benefit of using ServiceCollection
+        }
+
+        protected internal IConfigurationRoot ConfigurationRoot;
+
+        protected internal string OutputPath;
+    }
+}
